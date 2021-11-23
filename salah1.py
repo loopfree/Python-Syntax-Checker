601736ea90c2bc293b4ee7a9fbe551b1b8b08d78
Steven Wen <73114262+loopfree@users.noreply.github.com>
2021-11-23 21:22:42 +0700

    fix if, elif, else

--- salah1.py
+++ salah1.py
@@ -1,4 +1,24 @@
-if (a==b)
-{
-	print(a+b)
-}
+# if (a==b)
+# {
+# 	print(a+b)
+# }
+
+def do_something(x):
+    ''' This is a sample multiline comment
+    '''
+    if x > 0:
+        return 0
+    elif x + 4 > 1:
+        if True:
+            return 3
+        else:
+            return 2
+    elif x > 32:
+        return 4
+    else:
+        return 0
+
+# import pandas as pd
+# import matplotlib
+# from matplotlib import numpy
+# from matplotlib import numpy as np
